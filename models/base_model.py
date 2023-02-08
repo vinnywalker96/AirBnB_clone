6f9ad6762a20626981917299c1f3b5c7cc7d6697
vinnywalker96 <vinnywalker96@icloud.com>
2023-02-08 10:17:23 +0000

    quick fix base class

--- models/base_model.py
+++ models/base_model.py
@@ -17,32 +17,27 @@ class BaseModel:
             - **kwargs: dict of key-values arguments
         """
 
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, format)
                 else:
-                    self.__dict__[key] = kwargs[key]
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+                    storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
 
         return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+            format(__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at"""
 
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
         storage.save()
 
     def to_dict(self):
@@ -52,4 +47,5 @@ class BaseModel:
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        my_dict["__class__"] = self.__class__.__name__
         return my_dict
