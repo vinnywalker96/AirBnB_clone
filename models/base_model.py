02b9be2d2f3675b0bcd3542d1b910d878bf0ad8b
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2023-02-09 12:55:02 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -44,8 +44,7 @@ class BaseModel:
         """returns a dictionary containing all keys/values of __dict__"""
 
         my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
+        my_dict["__class__"] = self.__class__.__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        my_dict["__class__"] = self.__class__.__name__
         return my_dict
