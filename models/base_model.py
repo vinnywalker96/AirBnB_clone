418f7454d603b5ff3519609acc24e7619b3c4919
Vinny <vinnywalker96@icloud.com>
2023-02-13 15:13:38 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -16,18 +16,16 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-	if kwargs:
-	    for key, value in kwargs.items():
-	        if key == "created_at" or key == "updated_at":
-		    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-		if key != "__class__":
-		    setattr(self, key, value)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4)
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-                    
-
 
     def __str__(self):
         """Returns official string representation"""
