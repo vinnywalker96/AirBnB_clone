aa52d4a49f73eb8851a6089ca88898c16ada6e30
Vinny <vinnywalker96@icloud.com>
2023-02-13 15:04:31 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -16,17 +16,18 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
+	if kwargs:
+	    for key, value in kwargs.items():
+	        if key == "created_at" or key == "updated_at":
+		    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+		if key != "__class__":
+		    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4)
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+                    
 
-        format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, format)
-                else:
-                    storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
@@ -37,14 +38,14 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at"""
 
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
 
         my_dict = self.__dict__.copy()
-        my_dict["__class__"] = self.__class__.__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        my_dict["__class__"] = type(self).__nam__
+        my_dict["created_at"] = my_dict.created_at.isoformat()
+        my_dict["updated_at"] = my_dict.updated_at.isoformat()
         return my_dict
