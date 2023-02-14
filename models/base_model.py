22f3558fef36142a00ab2076e4ae892438186472
vinnywalker96 <vinnywalker96@icloud.com>
2023-02-14 14:29:58 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -42,7 +42,7 @@ class BaseModel:
         """returns a dictionary containing all keys/values of __dict__"""
 
         my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__nam__
-        my_dict["created_at"] = my_dict.created_at.isoformat()
-        my_dict["updated_at"] = my_dict.updated_at.isoformat()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
         return my_dict
