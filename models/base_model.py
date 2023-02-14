17f1f6bf9c5384ff3bcfffa6d1c8b3da64fe9414
vinnywalker96 <vinnywalker96@icloud.com>
2023-02-14 15:01:40 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,8 @@ class BaseModel:
     def __str__(self):
         """Returns official string representation"""
 
-        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at"""
